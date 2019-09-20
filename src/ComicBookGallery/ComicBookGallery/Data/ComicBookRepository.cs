6a4c888913621ac70a1fbf4e50d8ab4151e567de
linjj93 <linjj93@hotmail.com>
2019-09-20 23:59:19 +0800

    added comic books list view

--- src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
@@ -72,5 +72,10 @@ namespace ComicBookGallery.Data
             }
             return comicBook;
         }
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
     }
 }
